0efd4ba520dee740172a3a1a4248a29f0c0cb946
massie <massie>
2002-06-19 23:28:50 +0000

    Initial revision

---
+++ monitor-core/lib/become_a_nobody.c
@@ -0,0 +1,31 @@
+#include <ganglia/become_a_nobody.h>
+#include <ganglia/error.h>
+#include <stdio.h>
+
+void
+become_a_nobody( const char *username )
+{
+   int rval;
+   struct passwd *pw;
+ 
+   pw = getpwnam(username);
+   if ( pw == NULL )
+     {
+       err_quit("user '%s' does not exist\n\n", username);
+     }
+
+   rval = getuid();
+   if ( rval != pw->pw_uid )  
+     {
+       if ( rval != 0 )
+         {
+           err_quit("Must be root to setuid to \"%s\"\n\n", username);
+         }
+
+       rval = setuid(pw->pw_uid); 
+       if ( rval < 0 )
+         {
+           err_quit("exiting. setuid '%s' error", username);
+         }
+     }
+}
