8dfbd783e920b8ceabc97e8eff4b6162295d8c10
massie <massie@93a4e39c-3214-0410-bb16-828d8e3bcd0f>
2002-06-19 23:28:50 +0000

    Initial revision

---
+++ gmond/gmond.c
@@ -0,0 +1,190 @@
+#include "gangliaconf.h"
+#include "dotconf.h"
+#include <ganglia/gmond_config.h>
+#include <ganglia/hash.h>
+#include <ganglia/barrier.h>
+#include <ganglia/become_a_nobody.h>
+#include "metric.h"
+#include <pwd.h>
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#include "cmdline.h"
+
+/* The entire cluster this gmond knows about */
+hash_t *cluster;
+
+int             mcast_fd;
+pthread_mutex_t mcast_fd_mutex      = PTHREAD_MUTEX_INITIALIZER;
+
+int             mcast_join_fd;
+pthread_mutex_t mcast_join_fd_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int             server_fd;
+pthread_mutex_t server_fd_mutex     = PTHREAD_MUTEX_INITIALIZER;
+
+/* In dmonitor.c */
+extern void *monitor_thread(void *arg);
+extern int mcast_value ( uint32_t key );
+
+/* In dlisten.c */
+extern void *mcast_listen_thread(void *arg);  
+
+/* In dserver.c */
+extern void *server_thread(void *arg);
+
+/* In debug_msg.c */
+extern int debug_level;
+
+struct gengetopt_args_info args_info;
+
+gmond_config_t config;
+
+uint32_t start_time;
+
+void 
+send_all_metric_data( void )
+{
+   uint32_t i;
+ 
+   for (i=1; i< num_key_metrics; i++)
+      {
+         metric[i].mcast_threshold = 0;
+      }
+}              
+
+/* Added temporarily to make gexec work until I build the service model */
+g_val_t
+gexec_func ( void )
+{
+   g_val_t val;
+
+   if( config.no_gexec )
+      snprintf(val.str, MAX_G_STRING_SIZE, "%s", "OFF");
+   else
+      snprintf(val.str, MAX_G_STRING_SIZE, "%s", "ON");
+
+   return val;
+}
+
+g_val_t
+heartbeat_func( void )
+{
+   g_val_t val;
+
+   val.uint32 = start_time;
+   debug_msg("my start_time is %d\n", val.uint32);
+   return val;
+}
+
+int 
+main ( int argc, char *argv[] )
+{
+   int rval, i;
+   g_val_t initval;
+   pthread_t tid;
+   pthread_attr_t attr;
+   barrier *mcast_listen_barrier, *server_barrier;
+   struct timeval tv;
+
+   gettimeofday(&tv, NULL);
+   start_time = (uint32_t) tv.tv_sec;
+
+   if (cmdline_parser (argc, argv, &args_info) != 0)
+      exit(1) ;
+
+   rval = gmond_config(&config, args_info.conf_arg);
+   if ( rval == 0 )
+      {
+         debug_msg("no config file found.. going with defaults");
+      } 
+   else if( rval == 1)
+      {
+         debug_msg("config file %s processed with no errors", args_info.conf_arg);
+      }
+   else
+      {
+         err_quit("failed to process %s. Exiting.", args_info.conf_arg);
+      }
+
+   if(!config.no_setuid)
+      become_a_nobody(config.setuid);
+
+   debug_level = config.debug_level;
+   if (! debug_level )
+      {
+         daemon_init ( argv[0], 0);
+      }
+
+   pthread_attr_init( &attr );
+   pthread_attr_setdetachstate( &attr, PTHREAD_CREATE_DETACHED );
+
+   debug_msg("creating cluster has for %d nodes", config.num_nodes);
+   cluster = hash_create(config.num_nodes);
+   debug_msg("gmond initialized cluster hash");
+
+   srand(1);
+   
+   /* fd for incoming multicast messages */
+   if(! config.deaf )
+      {
+         mcast_join_fd = mcast_join(config.mcast_channel, config.mcast_port, config.mcast_if);
+         if ( mcast_join_fd == SYNAPSE_FAILURE)
+            {
+               perror("gmond could not join the multicast channel");
+               return -1;
+            }
+         debug_msg("mcast listening on %d %s %hu", mcast_join_fd, config.mcast_channel, config.mcast_port); 
+
+         server_fd = tcp_listen( config.xml_port );
+         if ( server_fd <0 )
+            {
+               perror("tcp_listen() on xml_port failed");
+               return -1;
+            }      
+         debug_msg("XML listening on port %d", config.xml_port);
+
+         /* thread(s) to listen to the multicast traffic */
+         barrier_init(&mcast_listen_barrier, config.mcast_threads);
+         for ( i = 0 ; i < config.mcast_threads; i++ )
+            {
+               pthread_create(&tid, &attr, mcast_listen_thread, (void *)mcast_listen_barrier);
+            }
+         debug_msg("listening thread(s) have been started");
+
+         /* threads to answer requests for XML */
+         barrier_init(&server_barrier, (config.xml_threads));
+         for ( i=0 ; i < config.xml_threads; i++ )
+            {
+               pthread_create(&tid, &attr, server_thread, (void *)server_barrier);
+            }
+         debug_msg("listening thread(s) have been started");
+      }
+
+   /* fd for outgoing multicast messages */
+   if(! config.mute )
+      {
+         mcast_fd = mcast_connect( config.mcast_channel, config.mcast_port, 
+                                   config.mcast_if, config.mcast_ttl);
+         if ( mcast_fd == SYNAPSE_FAILURE)
+            {
+               perror("gmond could not connect to multicast channel");
+               return -1;
+            }
+         debug_msg("multicasting on channel %s %d", config.mcast_channel, config.mcast_port);
+
+         /* in machine.c */
+         initval = metric_init();
+         if ( initval.int32 <0)
+            {
+               err_quit("monitor_init() returned an error");
+            }
+
+         pthread_create(&tid, &attr, monitor_thread, NULL);
+         debug_msg("created monitor thread");
+      }
+
+   for(;;)
+      pause();
+   return 0;
+}
---
+++ lib/daemon_init.c
@@ -0,0 +1,45 @@
+/**
+ * @file daemon_init.c Functions for standalone daemons
+ */ 
+#include	"gangliaconf.h"
+#include <ganglia/daemon_init.h>
+
+#define	MAXFD	64
+
+extern int daemon_proc;		/* defined in error.c */
+
+/**
+ * @fn void daemon_init (const char *pname, int facility)
+ * @param pname The name of your program
+ * @param facility See the openlog() manpage for details
+ */
+void
+daemon_init (const char *pname, int facility)
+{
+   int i;
+   pid_t pid;
+
+   pid = fork();
+
+   if (pid != 0)
+      exit (0);			/* parent terminates */
+
+   /* 41st child continues */
+   setsid ();			/* become session leader */
+
+   signal (SIGHUP, SIG_IGN);
+   if ((pid = fork ()) != 0)
+      exit (0);			/* 1st child terminates */
+
+   /* 42nd child continues */
+   daemon_proc = 1;		/* for our err_XXX() functions */
+
+   chdir ("/");			/* change working directory */
+
+   umask (0);			/* clear our file mode creation mask */
+
+   for (i = 0; i < MAXFD; i++)
+      close (i);
+
+   openlog (pname, LOG_PID, facility);
+}
