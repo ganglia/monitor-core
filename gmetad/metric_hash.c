f3b296d39890c9b844be98e56c4941d8bc150da1
massie <massie@93a4e39c-3214-0410-bb16-828d8e3bcd0f>
2002-08-31 01:46:34 +0000

    Work work work work work

--- gmetad/metric_hash.c
+++ gmetad/metric_hash.c
@@ -1,12 +1,7 @@
 /* C code produced by gperf version 2.7.2 */
-/* Command-line: gperf -G -l -k 1,4,7 -H metric_hash -N in_metric_list -W metrics ./metric_list  */
+/* Command-line: gperf -G -l -H metric_hash -t -F ', 0' -N in_metric_list -k '1,6,$' -W metrics ./metric_list  */
+/* $Id$ */
 #include <gmetad.h>
-
-#define TOTAL_KEYWORDS 43
-#define MIN_WORD_LENGTH 3
-#define MAX_WORD_LENGTH 13
-#define MIN_HASH_VALUE 6
-/* maximum key range = 92, duplicates = 0 */
 
 #ifdef __GNUC__
 __inline
@@ -22,137 +17,99 @@ metric_hash (str, len)
 {
   static unsigned char asso_values[] =
     {
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98,  0, 98, 35,  0,  0,
-       0, 15,  0, 10,  5, 35, 98, 60, 10, 30,
-      50, 10,  0, 98,  0, 40, 35, 30, 98,  5,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
-      98, 98, 98, 98, 98, 98
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64,  5, 64, 30, 20,  0,
+      23,  0,  0, 64, 10, 18, 64, 64,  0,  0,
+      29, 15,  0, 64, 10, 10, 27,  0, 64,  0,
+      64,  5, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+      64, 64, 64, 64, 64, 64
     };
-  register int hval = len;
-
-  switch (hval)
-    {
-      default:
-      case 7:
-        hval += asso_values[(unsigned char)str[6]];
-      case 6:
-      case 5:
-      case 4:
-        hval += asso_values[(unsigned char)str[3]];
-      case 3:
-      case 2:
-      case 1:
-        hval += asso_values[(unsigned char)str[0]];
-        break;
-    }
-  return hval;
+  return len + asso_values[(unsigned char)str[5]] + asso_values[(unsigned char)str[0]] + asso_values[(unsigned char)str[len - 1]];
 }
 
 static unsigned char lengthtable[] =
   {
-     0,  0,  0,  0,  0,  0,  6,  0,  8,  0,  0,  6,  0,  0,
-     0,  0,  0,  7,  8,  0, 10,  0,  0,  8,  9, 10, 11,  0,
-     0,  9,  5,  0,  0,  3,  9, 10,  0,  7,  8,  0, 10, 11,
-     0,  0,  0, 10,  0,  0,  0,  9, 10,  0,  0,  8,  9, 10,
-     0, 12,  8,  9, 10,  0, 12,  8,  0,  0,  0,  0,  8,  9,
-     0,  0,  0,  8,  9, 10,  0,  0,  8,  0, 10,  0,  7, 13,
-     9,  0,  0,  0,  0,  0,  0,  0,  0,  0,  9,  0,  0,  7
+     0,  0,  0,  0,  0,  0,  6,  0,  0,  9, 10,  0,  0,  0,
+     9, 10,  6,  0,  8,  9,  0, 11,  0,  8,  0,  0,  8,  0,
+     8,  0, 10,  8,  9, 10,  9,  0, 13, 10, 11,  0,  7, 12,
+     0, 10,  0,  0,  0,  8,  0,  0,  8,  0,  0,  0,  7,  0,
+     0,  0,  0,  0, 10,  9,  8, 10
   };
 
-static const char * metrics[] =
+struct ganglia_metric metrics[] =
   {
-    "", "", "", "", "", "",
-    "rcache",
-    "",
-    "cpu_nice",
-    "", "",
-    "wcache",
-    "", "", "", "", "",
-    "cpu_wio",
-    "cpu_idle",
-    "",
-    "proc_total",
-    "", "",
-    "cpu_user",
-    "cpu_speed",
-    "phread_sec",
-    "phwrite_sec",
-    "", "",
-    "heartbeat",
-    "gexec",
-    "", "",
-    "mtu",
-    "bytes_out",
-    "os_release",
-    "",
-    "cpu_num",
-    "proc_run",
-    "",
-    "mem_cached",
-    "mem_buffers",
-    "", "", "",
-    "bwrite_sec",
-    "", "", "",
-    "swap_free",
-    "cpu_system",
-    "", "",
-    "mem_free",
-    "load_five",
-    "lwrite_sec",
-    "",
-    "load_fifteen",
-    "bytes_in",
-    "sys_clock",
-    "swap_total",
-    "",
-    "machine_type",
-    "location",
-    "", "", "", "",
-    "load_one",
-    "disk_free",
-    "", "", "",
-    "boottime",
-    "mem_total",
-    "mem_shared",
-    "", "",
-    "pkts_out",
-    "",
-    "disk_total",
-    "",
-    "os_name",
-    "part_max_used",
-    "bread_sec",
-    "", "", "", "", "", "", "", "", "",
-    "lread_sec",
-    "", "",
-    "pkts_in"
+    {"", 0}, {"", 0}, {"", 0}, {"", 0}, {"", 0}, {"", 0},
+    {"wcache", FLOAT},
+    {"", 0}, {"", 0},
+    {"load_five", FLOAT},
+    {"lwrite_sec", FLOAT},
+    {"", 0}, {"", 0}, {"", 0},
+    {"lread_sec", FLOAT},
+    {"cpu_system", FLOAT},
+    {"rcache", FLOAT},
+    {"", 0},
+    {"mem_free", UINT32},
+    {"swap_free", UINT32},
+    {"", 0},
+    {"mem_buffers", UINT32},
+    {"", 0},
+    {"load_one", FLOAT},
+    {"", 0}, {"", 0},
+    {"cpu_nice", FLOAT},
+    {"", 0},
+    {"cpu_user", FLOAT},
+    {"", 0},
+    {"bwrite_sec", FLOAT},
+    {"cpu_idle", FLOAT},
+    {"disk_free", DOUBLE},
+    {"phread_sec", FLOAT},
+    {"bread_sec", FLOAT},
+    {"", 0},
+    {"part_max_used", FLOAT},
+    {"proc_total", UINT32},
+    {"phwrite_sec", FLOAT},
+    {"", 0},
+    {"cpu_wio", FLOAT},
+    {"load_fifteen", FLOAT},
+    {"", 0},
+    {"mem_shared", UINT32},
+    {"", 0}, {"", 0}, {"", 0},
+    {"proc_run", UINT32},
+    {"", 0}, {"", 0},
+    {"pkts_out", FLOAT},
+    {"", 0}, {"", 0}, {"", 0},
+    {"pkts_in", FLOAT},
+    {"", 0}, {"", 0}, {"", 0}, {"", 0}, {"", 0},
+    {"disk_total", DOUBLE},
+    {"bytes_out", FLOAT},
+    {"bytes_in", FLOAT},
+    {"mem_cached", UINT32}
   };
 
 #ifdef __GNUC__
 __inline
 #endif
-const char *
+struct ganglia_metric *
 in_metric_list (str, len)
      register const char *str;
      register unsigned int len;
@@ -164,10 +121,10 @@ in_metric_list (str, len)
       if (key <= MAX_HASH_VALUE && key >= 0)
         if (len == lengthtable[key])
           {
-            register const char *s = metrics[key];
+            register const char *s = metrics[key].name;
 
             if (*str == *s && !memcmp (str + 1, s + 1, len - 1))
-              return s;
+              return &metrics[key];
           }
     }
   return 0;
