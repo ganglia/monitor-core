d2937de0c18a1f25616fa03fec4d5ef2d21180e3
massie <massie>
2002-09-04 04:20:37 +0000

    *** empty log message ***

--- monitor-core/gmetad/data_thread.c
+++ monitor-core/gmetad/data_thread.c
@@ -136,7 +136,7 @@ data_thread ( void *arg )
          rval = process_xml(d->index, d->name, buf );
          if(rval)
             {
-               debug_msg("save_to_rrd() couldn't parse the XML");
+               debug_msg("save_to_rrd() couldn't parse the XML for [%s]", d->name);
                goto take_a_break;
             }    
 
