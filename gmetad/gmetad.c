86438372da58d0e68aaef067877f0a22b099a9b9
massie <massie>
2002-09-05 01:03:22 +0000

    *** empty log message ***

--- gmetad/gmetad.c
+++ gmetad/gmetad.c
@@ -24,6 +24,8 @@ extern void* server_thread(void *);
 extern int parse_config_file ( char *config_file );
 extern int number_of_datasources ( char *config_file );
 
+extern struct ganglia_metric metrics[];
+
 llist_entry *trusted_hosts = NULL;
 extern int debug_level;
 int xml_port = 8651;
@@ -36,6 +38,9 @@ int should_setuid = 1;
 unsigned int source_index = 0;
 
 struct gengetopt_args_info args_info;
+
+long double  sum_of_sums[MAX_METRIC_HASH_VALUE]; 
+unsigned int sum_of_nums[MAX_METRIC_HASH_VALUE];
 
 int
 print_sources ( datum_t *key, datum_t *val, void *arg )
@@ -69,15 +74,41 @@ spin_off_the_data_threads( datum_t *key, datum_t *val, void *arg )
 }
 
 int
+save_sums_n_nums( datum_t *key, datum_t *val, void *arg )
+{
+   data_source_list_t *ds =  *(data_source_list_t **)val->data;
+   struct ganglia_metric *gm;
+   register int i;
+   int len;
+
+   /* Ignore dead data sources */
+   if( ds->dead )
+      {
+         debug_msg("[%s] is a dead source", ds->name);
+         return 0;
+      }
+ 
+   for ( i = 0; i < MAX_METRIC_HASH_VALUE; i++ )
+      {
+         sum_of_sums[i] += ds->sum[i];
+         sum_of_nums[i] += ds->num[i];
+      }
+
+   return 0;
+}
+
+int
 main ( int argc, char *argv[] )
 {
    struct stat struct_stat;
    pthread_t pid;
    pthread_attr_t attr;
-   int i, num_sources;
+   int i, num_sources, sleep_time;
    uid_t gmetad_uid;
    char * gmetad_username;
    struct passwd *pw;
+   char sum[512];
+   char num[512];  
 
    srand(52336789);
 
@@ -166,9 +197,34 @@ main ( int argc, char *argv[] )
 
    hash_foreach( sources, spin_off_the_data_threads, NULL );
 
-   /* Put in RRD processing here */
+   /* Meta data */
    for(;;)
-      pause();
+      {
+         sleep_time=10+(int) (20.0*rand()/(RAND_MAX+10.0));
+         sleep(sleep_time);
 
+         /* Flush the old values */
+         memset( &sum_of_sums, 0, MAX_METRIC_HASH_VALUE * sizeof( long double ));
+         memset( &sum_of_nums, 0, MAX_METRIC_HASH_VALUE * sizeof( unsigned int));
+
+         /* Sum the new values */
+         hash_foreach ( sources, save_sums_n_nums, NULL );
+
+         /* Save them to RRD */
+         for ( i = 0; i < MAX_METRIC_HASH_VALUE; i++ )
+            {
+               if( strlen(metrics[i].name) && sum_of_nums[i])
+                  {
+                     sprintf(num, "%d",  sum_of_nums[i]);
+                     sprintf(sum, "%Lf", sum_of_sums[i]);
+
+                     /* Save the data to a round robin database */
+                     if( write_data_to_rrd( NULL, NULL, (char *)metrics[i].name, sum, num, "15") )
+                        {
+                           err_msg("Unable to write meta data to RRDbs");
+                        }                     
+                  }
+            }
+      }
    return 0;
 }
