c8f351118ceff8ca44606b99b3056425d0d45fb1
massie <massie@93a4e39c-3214-0410-bb16-828d8e3bcd0f>
2004-02-19 00:51:08 +0000

    Moving the web frontend source into the monitor-core distribution

---
+++ web/get_context.php
@@ -0,0 +1,126 @@
+<?php
+/* $Id$ */
+
+$meta_designator = "Grid";
+$cluster_designator = "Cluster Overview";
+
+# Blocking malicious CGI input.
+$clustername = escapeshellcmd(rawurldecode($HTTP_GET_VARS["c"]));
+$gridname = escapeshellcmd(rawurldecode($HTTP_GET_VARS["G"]));
+$hostname = escapeshellcmd(rawurldecode($HTTP_GET_VARS["h"]));
+$range = escapeshellcmd(rawurldecode($HTTP_GET_VARS["r"]));
+$metricname = escapeshellcmd(rawurldecode($HTTP_GET_VARS["m"]));
+$sort = escapeshellcmd(rawurldecode($HTTP_GET_VARS["s"]));
+$controlroom = escapeshellcmd(rawurldecode($HTTP_GET_VARS["cr"]));
+$hostcols = escapeshellcmd($HTTP_GET_VARS["hc"]);
+$showhosts = escapeshellcmd($HTTP_GET_VARS["sh"]);
+# The 'p' variable specifies the verbosity level in the physical view.
+$physical = escapeshellcmd($HTTP_GET_VARS["p"]);
+$tree = escapeshellcmd($HTTP_GET_VARS["t"] );
+# A custom range value for job graphs, in -sec.
+$jobrange = escapeshellcmd($HTTP_GET_VARS["jr"]);
+# A red vertical line for various events. Value specifies the event time.
+$jobstart = escapeshellcmd($HTTP_GET_VARS["js"]);
+# The direction we are travelling in the grid tree
+$gridwalk = escapeshellcmd($HTTP_GET_VARS["gw"]);
+# A stack of grid parents. Prefer a GET variable, default to cookie.
+if ($HTTP_GET_VARS["gs"])
+      $gridstack = explode(">", rawurldecode($HTTP_GET_VARS["gs"]));
+else
+      $gridstack = explode(">", $HTTP_COOKIE_VARS["gs"] );
+# Assume we are the first grid visited in the tree if there are no CGI variables,
+# or gridstack is not well formed. Gridstack always has at least one element.
+if (!count($HTTP_GET_VARS) or !strstr($gridstack[0], "http://"))
+      $initgrid=TRUE;
+
+# Default values
+if (!is_numeric($hostcols)) $hostcols = 4;
+if (!is_numeric($showhosts)) $showhosts = 1;
+
+# Set context.
+if(!$clustername && !$hostname && $controlroom)
+   {
+      $context = "control";
+   }
+else if ($tree)
+   {
+      $context = "tree";
+   }
+else if(!$clustername and !$gridname and !$hostname)
+   {
+      $context = "meta";
+   }
+else if($gridname)
+   {
+      $context = "grid";
+   }
+else if ($clustername and !$hostname and $physical)
+   {
+      $context = "physical";
+   }
+else if ($clustername and !$hostname and !$showhosts)
+   {
+      $context = "cluster-summary";
+   }
+else if($clustername and !$hostname)
+   {
+      $context = "cluster";
+   }
+else if($clustername and $hostname and $physical)
+   {
+      $context = "node";
+   }
+else if($clustername and $hostname)
+   {
+      $context = "host";
+   }
+
+if (!$range)
+      $range = "$default_range";
+
+$end = "N";
+
+switch ($range)
+{
+   case "hour":  $start = -3600; break;
+   case "day":   $start = -86400; break;
+   case "week":  $start = -604800; break;
+   case "month": $start = -2419200; break;
+   case "year":  $start = -31449600; break;
+   case "job":
+      if ($jobrange) {
+        $start = $jobrange;
+        break;
+     }
+   default: $start = -3600;
+}
+
+if (!$metricname)
+      $metricname = "$default_metric";
+
+if (!$sort)
+      $sort = "descending";
+
+# A hack for pre-2.5.0 ganglia data sources.
+$always_constant = array(
+   "swap_total" => 1,
+   "cpu_speed" => 1,
+   "swap_total" => 1
+);
+
+$always_timestamp = array(
+   "gmond_started" => 1,
+   "reported" => 1,
+   "sys_clock" => 1,
+   "boottime" => 1
+);
+
+# List of report graphs
+$reports = array(
+   "load_report" => "load_one",
+   "cpu_report" => 1,
+   "mem_report" => 1,
+   "network_report" => 1
+);
+
+?>
---
+++ web/header.php
@@ -0,0 +1,317 @@
+<?php
+/* $Id$ */
+
+# Check if this context is private.
+include_once "auth.php";
+checkcontrol();
+checkprivate();
+
+if ( $context == "control" && $controlroom < 0 )
+      $header = "header-nobanner";
+else
+      $header = "header";
+
+$tpl = new TemplatePower( template("$header.tpl") );
+$tpl->prepare();
+
+# Maintain our path through the grid tree.
+$me = $self . "@" . $grid[$self][AUTHORITY];
+if ($initgrid)
+   {
+      $gridstack = array();
+      $gridstack[] = $me;
+   }
+else if ($gridwalk=="fwd")
+   {
+      # push our info on gridstack, format is "name@url>name2@url".
+      if (end($gridstack) != $me)
+         {
+            $gridstack[] = $me;
+         }
+   }
+else if ($gridwalk=="back")
+   {
+      # pop a single grid off stack.
+      if (end($gridstack) != $me)
+         {
+            array_pop($gridstack);
+         }
+   }
+$gridstack_str = join(">", $gridstack);
+$gridstack_url = rawurlencode($gridstack_str);
+
+if ($initgrid or $gridwalk)
+   {
+      # Use cookie so we dont have to pass gridstack around within this site.
+      # Cookie values are automatically urlencoded. Expires in a day.
+      setcookie("gs", $gridstack_str, time() + 86400);
+   }
+
+# Invariant: back pointer is second-to-last element of gridstack. Grid stack never
+# has duplicate entries.
+list($parentgrid, $parentlink) = explode("@", $gridstack[count($gridstack)-2]);
+
+# Setup a redirect to a remote server if you choose a grid from pulldown menu. Tell
+# destination server that we're walking foward in the grid tree.
+if (strstr($clustername, "http://")) 
+   {
+      $tpl->assign("refresh", "0");
+      $tpl->assign("redirect", ";URL=$clustername?gw=fwd&gs=$gridstack_url");
+      echo "<h2>Redirecting, please wait...</h2>";
+      $tpl->printToScreen();
+      exit;
+   }
+$tpl->assign("refresh", $default_refresh);
+
+$tpl->assign( "date", date("r"));
+$tpl->assign( "page_title", $title );
+
+# The page to go to when "Get Fresh Data" is pressed.
+if (isset($page))
+      $tpl->assign("page",$page);
+else
+      $tpl->assign("page","./");
+
+# Templated Logo image
+$tpl->assign("images","./templates/$template_name/images");
+
+#
+# Used when making graphs via graph.php. Included in most URLs
+#
+$sort_url=rawurlencode($sort);
+$get_metric_string = "m=$metric&r=$range&s=$sort_url&hc=$hostcols";
+if ($jobrange and $jobstart)
+        $get_metric_string .= "&jr=$jobrange&js=$jobstart";
+
+# Set the Alternate view link.
+$cluster_url=rawurlencode($clustername);
+$node_url=rawurlencode($hostname);
+
+if ($context=="cluster")
+   {
+      $tpl->assign("alt_view", "<a href=\"./?p=2&c=$cluster_url\">Physical View</a>");
+   }
+elseif ($context=="physical")
+   {
+      $tpl->assign("alt_view", "<a href=\"./?c=$cluster_url\">Full View</a>");
+   }
+elseif ($context=="node")
+   {
+      $tpl->assign("alt_view",
+      "<a href=\"./?c=$cluster_url&h=$node_url&$get_metric_string\">Host View</a>");
+   }
+elseif ($context=="host")
+   {
+      $tpl->assign("alt_view",
+      "<a href=\"./?p=2&c=$cluster_url&h=$node_url\">Node View</a>");
+   }
+
+# Build the node_menu
+$node_menu = "";
+
+if ($parentgrid) 
+   {
+      $node_menu .= "<B><A HREF=\"$parentlink?gw=back&gs=$gridstack_url\">".
+         "$parentgrid $meta_designator</A></B> ";
+      $node_menu .= "<B>&gt;</B>\n";
+   }
+
+# Show grid.
+$mygrid =  ($self == "unspecified") ? "" : $self;
+$node_menu .= "<B><A HREF=\"./?$get_metric_string\">$mygrid $meta_designator</A> ";
+$node_menu .= "<B>&gt;</B>\n";
+
+if ($physical)
+   $node_menu .= hiddenvar("p", $physical);
+
+if ( $clustername )
+   {
+      $url = rawurlencode($clustername);
+      $node_menu .= "<B><A HREF=\"./?c=$url&$get_metric_string\">$clustername</A></B> ";
+      $node_menu .= "<B>&gt;</B>\n";
+      $node_menu .= hiddenvar("c", $clustername);
+   }
+else
+   {
+      # No cluster has been specified, so drop in a list
+      $node_menu .= "<SELECT NAME=\"c\" OnChange=\"ganglia_form.submit();\">\n";
+      $node_menu .= "<OPTION VALUE=\"\">--Choose a Source\n";
+      ksort($grid);
+      foreach( $grid as $k => $v )
+         {
+            if ($k==$self) continue;
+            if ($v[GRID])
+               {
+                  $url = $v[AUTHORITY];
+                  $node_menu .="<OPTION VALUE=\"$url\">$k $meta_designator\n";
+               }
+            else
+               {
+                  $url = rawurlencode($k);
+                  $node_menu .="<OPTION VALUE=\"$url\">$k\n";
+               }
+         }
+      $node_menu .= "</SELECT>\n";
+   }
+
+if ( $clustername && !$hostname )
+   {
+      # Drop in a host list if we have hosts
+      if(is_array($hosts_up) || is_array($hosts_down))
+         {
+            $node_menu .= "<SELECT NAME=\"h\" OnChange=\"ganglia_form.submit();\">";
+            $node_menu .= "<OPTION VALUE=\"\">--Choose a Node\n";
+            if(is_array($hosts_up))
+               {
+                  ksort($hosts_up);
+                  foreach($hosts_up as $k=> $v)
+                     {
+                        $url = rawurlencode($k);
+                        $node_menu .= "<OPTION VALUE=\"$url\">$k\n";
+                     }
+               }
+            if(is_array($hosts_down))
+               {
+                  ksort($hosts_down);
+                  foreach($hosts_down as $k=> $v)
+                     {
+                        $url = rawurlencode($k);
+                        $node_menu .= "<OPTION VALUE=\"$url\">$k\n";
+                     }
+               }
+            $node_menu .= "</SELECT>\n";
+         }
+      else
+         {
+            $node_menu .= "<B>No Hosts</B>\n";
+         }
+   }
+else
+   {
+      $node_menu .= "<B>$hostname</B>\n";
+      $node_menu .= hiddenvar("h", $hostname);
+   }
+
+# Save other CGI variables
+$node_menu .= hiddenvar("cr", $controlroom);
+$node_menu .= hiddenvar("js", $jobstart);
+$node_menu .= hiddenvar("jr", $jobrange);
+
+$tpl->assign("node_menu", $node_menu);
+
+
+//////////////////// Build the metric menu ////////////////////////////////////
+
+if( $context == "cluster" )
+   {
+   if (!count($metrics)) {
+      echo "<h4>Cannot find any metrics for selected cluster \"$clustername\", exiting.</h4>\n";
+      echo "Check ganglia XML tree (telnet $ganglia_ip $ganglia_port)\n";
+      exit;
+   }
+   $firsthost = key($metrics);
+   foreach ($metrics[$firsthost] as $m => $foo)
+         $context_metrics[] = $m;
+   foreach ($reports as $r => $foo)
+         $context_metrics[] = $r;
+   }
+
+#
+# If there are graphs present, show ranges.
+#
+if (!$physical) {
+   $context_ranges[]="hour";
+   $context_ranges[]="day";
+   $context_ranges[]="week";
+   $context_ranges[]="month";
+   $context_ranges[]="year";
+   if ($jobrange)
+      $context_ranges[]="job";
+
+   $range_menu = "<B>Last</B>&nbsp;&nbsp;"
+      ."<SELECT NAME=\"r\" OnChange=\"ganglia_form.submit();\">\n";
+   foreach ($context_ranges as $v) {
+      $url=rawurlencode($v);
+      $range_menu .= "<OPTION VALUE=\"$url\" ";
+      if ($v == $range)
+         $range_menu .= "SELECTED";
+      $range_menu .= ">$v\n";
+   }
+   $range_menu .= "</SELECT>\n";
+
+   $tpl->assign("range_menu", $range_menu);
+}
+
+#
+# Only show metric list if we have some and are in cluster context.
+#
+if (is_array($context_metrics) and $context == "cluster")
+   {
+      $metric_menu = "<B>Metric</B>&nbsp;&nbsp;"
+         ."<SELECT NAME=\"m\" OnChange=\"ganglia_form.submit();\">\n";
+
+      sort($context_metrics);
+      foreach( $context_metrics as $k )
+         {
+            $url = rawurlencode($k);
+            $metric_menu .= "<OPTION VALUE=\"$url\" ";
+            if ($k == $metricname )
+                  $metric_menu .= "SELECTED";
+            $metric_menu .= ">$k\n";
+         }
+      $metric_menu .= "</SELECT>\n";
+
+      $tpl->assign("metric_menu", $metric_menu );      
+   }
+
+
+#
+# Show sort order if there is more than one physical machine present.
+#
+if ($context == "meta" or $context == "cluster")
+   {
+      $context_sorts[]="ascending";
+      $context_sorts[]="descending";
+      $context_sorts[]="by hostname";
+
+      $sort_menu = "<B>Sorted</B>&nbsp;&nbsp;"
+         ."<SELECT NAME=\"s\" OnChange=\"ganglia_form.submit();\">\n";
+      foreach ( $context_sorts as $v )
+         {
+            $url = rawurlencode($v);
+            $sort_menu .= "<OPTION VALUE=\"$url\" ";
+            if ($v == $sort )
+                  $sort_menu .= "SELECTED";
+
+            $sort_menu .= ">$v\n";
+         }
+      $sort_menu .= "</SELECT>\n";
+
+      $tpl->assign("sort_menu", $sort_menu );
+   }
+   
+if ($context == "physical" or $context == "cluster")
+   {
+      # Present a width list
+      $cols_menu = "<SELECT NAME=\"hc\" OnChange=\"ganglia_form.submit();\">\n";
+
+      foreach(range(1,25) as $cols)
+         {
+            $cols_menu .= "<OPTION VALUE=$cols ";
+            if ($cols == $hostcols)
+               $cols_menu .= "SELECTED";
+            $cols_menu .= ">$cols\n";
+         }
+      $cols_menu .= "</SELECT>\n";
+
+      # Assign template variable in cluster view.
+   }
+
+# Make sure that no data is cached..
+header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    # Date in the past
+header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); # always modified
+header ("Cache-Control: no-cache, must-revalidate");  # HTTP/1.1
+header ("Pragma: no-cache");                          # HTTP/1.0
+
+$tpl->printToScreen();
+?>
