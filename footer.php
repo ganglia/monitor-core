d6b72ec59302f867238a5e51db50f37c0b4d1bed
massie <massie>
2004-02-24 18:40:05 +0000

    *** empty log message ***

--- footer.php
+++ footer.php
@@ -1,18 +1,12 @@
 <?php
+include_once "conf.php";
+
 /* $Id$ */
 $tpl = new TemplatePower( template("footer.tpl") );
 $tpl->prepare();
-$tpl->assign("webfrontend-version",$version["webfrontend"]);
 
-if ($version["gmetad"]) {
-   $tpl->assign("webbackend-component", "gmetad");
-   $tpl->assign("webbackend-version",$version["gmetad"]);
-}
-elseif ($version["gmond"]) {
-   $tpl->assign("webbackend-component", "gmond");
-   $tpl->assign("webbackend-version", $version["gmond"]);
-}
-
+$tpl->assign("ganglia_version",$ganglia_version);
+$tpl->assign("ganglia_release_name", $ganglia_release_name);
 $tpl->assign("parsetime", sprintf("%.4f", $parsetime) . "s");
 
 $tpl->printToScreen();
