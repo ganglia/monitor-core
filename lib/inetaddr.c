83141e41751321d940a3823ef612c4ed6bb188bb
massie <massie@93a4e39c-3214-0410-bb16-828d8e3bcd0f>
2004-11-18 21:42:46 +0000

    *** empty log message ***

--- lib/inetaddr.c
+++ lib/inetaddr.c
@@ -22,6 +22,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA  02111-1307, USA.
  */
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
--- lib/my_inet_ntop.c
+++ lib/my_inet_ntop.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 const char *
