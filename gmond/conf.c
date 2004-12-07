8b7d6b44367972a31f035563bffec357ef4ad144
massie <massie@93a4e39c-3214-0410-bb16-828d8e3bcd0f>
2004-12-07 02:42:34 +0000

    *** empty log message ***

--- gmond/conf.c
+++ gmond/conf.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
 #include "conf.h"
 
 
+/* This function is necessary only because I need
+ * to know if the user set metric thresholds */
 int metric_validate_func(cfg_t *cfg, cfg_opt_t *opt)
 {
-  fprintf(stderr,"Got called\n");
+  char buf[1024];
+  snprintf(buf, 1024,"%s_given", opt->name);
+  cfg_setbool(cfg, buf, 1);
   return 0;
 }
