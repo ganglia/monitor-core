9cfa4a4bfb483619cbbebe991b599c472faa7e95
massie <massie@93a4e39c-3214-0410-bb16-828d8e3bcd0f>
2004-12-10 20:15:21 +0000

    *** empty log message ***

--- lib/apr_net.h
+++ lib/apr_net.h
@@ -7,5 +7,8 @@ create_udp_client(apr_pool_t *context, char *ipaddr, apr_port_t port);
 apr_socket_t *
 create_udp_server(apr_pool_t *context, apr_port_t port, char *bind);
 
+APR_DECLARE(apr_status_t) apr_sockaddr_ip_buffer_get(char *addr, int len,
+                                         apr_sockaddr_t *sockaddr);
+
 
 #endif
