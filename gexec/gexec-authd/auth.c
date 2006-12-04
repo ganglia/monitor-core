d1bbb7f43743d2b20232c8d555ca8794059091db
efocht <efocht>
2006-12-04 22:17:05 +0000

    Updated version of authd (0.2.2). I renamed the package to gexec-authd because there is another authd in RedHat which has the function of an identd. The idea of renaming comes from Babu Sundaram (in the Google Summer of Code projects for OSCAR).

---
+++ gexec/gexec-authd/auth.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2002 Brent N. Chun <bnc@caltech.edu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+#include <e/e_error.h>
+#include <e/net.h>
+#include "auth.h"
+
+void auth_init_credentials(credentials *creds, int lifetime)
+{
+    time_t now;
+    
+    time(&now);
+    creds->uid        = getuid();
+    creds->gid        = getgid();
+    creds->valid_from = now;
+    creds->valid_to   = now + lifetime;
+}
+
+int auth_get_signature(credentials *creds, signature *creds_sig)
+{
+    int     sock;
+    char    auth_sock_path[UNIX_PATH_MAX];
+    
+    sprintf(auth_sock_path, AUTH_SOCK_PATH, getpid());
+    if (net_cli_unixsock_create(&sock, auth_sock_path,
+                                AUTHD_SOCK_PATH) != E_OK)
+        return AUTH_NET_ERROR;
+    if (net_send_bytes(sock, creds, sizeof(credentials)) != E_OK) {
+        close(sock);
+        return AUTH_NET_ERROR;
+    }
+    if (net_recv_bytes(sock, creds_sig, sizeof(signature)) != E_OK) {
+        close(sock);
+        return AUTH_NET_ERROR;
+    }
+    close(sock);
+    return AUTH_OK;
+}
+
+int auth_verify_signature(credentials *creds, signature *creds_sig)
+{
+    int     error;
+    RSA     *pub_key = NULL;
+    FILE    *fp = NULL;
+
+    if ((fp = fopen(AUTH_PUB_KEY, "r")) == NULL) {
+        error = AUTH_FOPEN_ERROR;
+        goto cleanup;
+    }
+    if ((pub_key = PEM_read_RSA_PUBKEY(fp, NULL, NULL, NULL)) == NULL) {
+        error = AUTH_RSA_ERROR;
+        goto cleanup;
+    }
+    ERR_load_crypto_strings();
+    if (RSA_verify(NID_sha1, (unsigned char *)creds, sizeof(credentials), 
+                   creds_sig->data, AUTH_RSA_SIGLEN, pub_key) == 0) {
+        error = AUTH_RSA_ERROR;
+        goto cleanup;
+    }
+    RSA_free(pub_key);
+    fclose(fp);
+    return AUTH_OK;
+
+ cleanup:    
+    if (pub_key != NULL)
+        RSA_free(pub_key);
+    if (fp != NULL) 
+        fclose(fp);
+    return error;
+}
---
+++ gexec/gexec-authd/authd.c
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2002 Brent N. Chun <bnc@caltech.edu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/tcp.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+#include <e/e_error.h>
+#include <e/net.h>
+#include "auth.h"
+#include "authd.h"
+#include "authd_options.h"
+
+static int check_credentials(struct ucred *ucred, credentials *cred)
+{
+    double lifetime;
+
+    if (ucred->uid != cred->uid || ucred->gid != cred->gid)
+        return AUTH_CRED_ERROR;
+    lifetime = difftime(cred->valid_to, cred->valid_from);
+    if (lifetime > AUTHD_CRED_MAXLIFE)
+        return AUTH_CRED_ERROR;
+    return AUTH_OK;
+}
+
+static int gen_signature(credentials *cred, signature *cred_sig)
+{
+    int     error;
+    int     sig_len;
+    RSA     *priv_key = NULL;
+    FILE    *fp = NULL;
+
+    if ((fp = fopen(AUTH_PRIV_KEY, "r")) == NULL) {
+        error = AUTH_FOPEN_ERROR;
+        goto cleanup;
+    }
+    if ((priv_key = PEM_read_RSAPrivateKey(fp, NULL, NULL, NULL)) == NULL) {
+        error = AUTH_RSA_ERROR;
+        goto cleanup;
+    }
+    if (RSA_sign(NID_sha1, (unsigned char *)cred, sizeof(credentials), 
+                 cred_sig->data, &sig_len, priv_key) == 0) {
+        error = AUTH_RSA_ERROR;
+        goto cleanup;
+    }
+    RSA_free(priv_key);
+    e_assert(sig_len == AUTH_RSA_SIGLEN);
+    fclose(fp);
+    return AUTH_OK;
+
+ cleanup:    
+    if (priv_key != NULL)
+        RSA_free(priv_key);
+    if (fp != NULL) 
+        fclose(fp);
+    return error;
+}
+
+static void *authd_thr(void *arg)
+{
+    long int     sock = (long int)arg;
+    int          len = sizeof(struct ucred); 
+    struct ucred ucred;
+    credentials  cred;
+    signature    cred_sig;
+
+    if (getsockopt(sock, SOL_SOCKET, SO_PEERCRED, &ucred, &len) < 0)
+        goto cleanup;
+    if (net_recv_bytes(sock, &cred, sizeof(credentials)) != E_OK)
+        goto cleanup;
+    if (check_credentials(&ucred, &cred) != AUTH_OK)
+        goto cleanup;
+    if (gen_signature(&cred, &cred_sig) != AUTH_OK)
+        goto cleanup;
+    if (net_send_bytes(sock, &cred_sig, sizeof(signature)) != E_OK)
+        goto cleanup;
+    close(sock);
+    return NULL;
+
+ cleanup:
+    close(sock);
+    return NULL;
+}
+
+int main(int argc, char **argv)
+{
+    authd_options       opts;
+    long int            svr_sock, cli_sock;
+    pthread_attr_t      attr;
+    pthread_t           tid;
+    struct sockaddr_un  sockaddr;
+    socklen_t           sockaddr_len = sizeof(struct sockaddr_un);
+
+    authd_options_init(&opts);
+    authd_options_parse(&opts, argc, argv);
+    if (opts.daemon)
+        daemon(0, 0);
+
+    signal(SIGPIPE, SIG_IGN);
+    if (net_svr_unixsock_create((int *)&svr_sock, AUTHD_SOCK_PATH) != E_OK) {
+        fprintf(stderr, "Bind error on Unix domain socket %s\n",
+                AUTHD_SOCK_PATH);
+        exit(2);
+    }
+
+    ERR_load_crypto_strings();
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+    for (;;) {
+        if ((cli_sock = accept(svr_sock, (struct sockaddr *)&sockaddr,
+                               &sockaddr_len)) < 0) {
+            if (errno == EINTR)
+                continue;
+            fatal_error("accept error\n");
+            exit(1);
+        }
+        if (pthread_create(&tid, &attr, authd_thr, (void *)cli_sock) != 0)
+            close(cli_sock);
+    }
+
+    pthread_attr_destroy(&attr);
+    close(svr_sock);
+    return 0;
+}
