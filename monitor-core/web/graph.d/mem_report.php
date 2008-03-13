7d8b66587fcd0fad254930398dac56fe0bf8335a
hawson <hawson>
2008-03-13 03:33:57 +0000

    Implement modular graphs.  All specific graph definitions are handled a single .php file.

---
+++ monitor-core/web/graph.d/mem_report.php
@@ -0,0 +1,58 @@
+<?php
+
+
+/* Pass in by reference! */
+function graph_mem_report ( &$rrdtool_graph ) {
+
+    global $context, 
+           $hostname, 
+           $mem_shared_color,
+           $mem_cached_color, 
+           $mem_buffered_color, 
+           $mem_swapped_color, 
+           $mem_used_color,
+           $cpu_num_color,
+           $rrd_dir,
+           $size;
+    
+    $rrdtool_graph['height']        += $size == 'medium' ? 19 : 0 ;  // Two lines of text in the legend
+    $rrdtool_graph['title']          = 'Memory';
+    $rrdtool_graph['lower-limit']    = '0';
+    $rrdtool_graph['vertical-label'] = 'Bytes';
+    $rrdtool_graph['extras']         = '--rigid --base 1024';
+
+    if ($context == 'host')
+        $rrdtool_graph['title'] .= " - $hostname";
+
+
+    $series = "DEF:'mem_total'='${rrd_dir}/mem_total.rrd':'sum':AVERAGE "
+        ."CDEF:'bmem_total'=mem_total,1024,* "
+        ."DEF:'mem_shared'='${rrd_dir}/mem_shared.rrd':'sum':AVERAGE "
+        ."CDEF:'bmem_shared'=mem_shared,1024,* "
+        ."DEF:'mem_free'='${rrd_dir}/mem_free.rrd':'sum':AVERAGE "
+        ."CDEF:'bmem_free'=mem_free,1024,* "
+        ."DEF:'mem_cached'='${rrd_dir}/mem_cached.rrd':'sum':AVERAGE "
+        ."CDEF:'bmem_cached'=mem_cached,1024,* "
+        ."DEF:'mem_buffers'='${rrd_dir}/mem_buffers.rrd':'sum':AVERAGE "
+        ."CDEF:'bmem_buffers'=mem_buffers,1024,* "
+        ."CDEF:'bmem_used'='bmem_total','bmem_shared',-,'bmem_free',-,'bmem_cached',-,'bmem_buffers',- "
+        ."AREA:'bmem_used'#$mem_used_color:'Memory Used' "
+        ."STACK:'bmem_shared'#$mem_shared_color:'Memory Shared' "
+        ."STACK:'bmem_cached'#$mem_cached_color:'Memory Cached' "
+        ."STACK:'bmem_buffers'#$mem_buffered_color:'Memory Buffered' ";
+    if (file_exists("$rrd_dir/swap_total.rrd")) {
+        $series .= "DEF:'swap_total'='${rrd_dir}/swap_total.rrd':'sum':AVERAGE "
+            ."DEF:'swap_free'='${rrd_dir}/swap_free.rrd':'sum':AVERAGE "
+            ."CDEF:'bmem_swapped'='swap_total','swap_free',-,1024,* "
+            ."STACK:'bmem_swapped'#$mem_swapped_color:'Memory Swapped' ";
+    }
+    $series .= "LINE2:'bmem_total'#$cpu_num_color:'Total In-Core Memory' ";
+
+  
+    $rrdtool_graph['series'] = $series;
+
+    return $rrdtool_graph;
+
+}
+
+?>
---
+++ monitor-core/web/graph.d/network_report.php
@@ -0,0 +1,35 @@
+<?php
+
+
+/* Pass in by reference! */
+function graph_network_report ( &$rrdtool_graph ) {
+
+    global $context, 
+           $hostname,
+           $mem_cached_color, 
+           $mem_used_color,
+           $cpu_num_color,
+           $rrd_dir,
+           $size;
+    
+    $rrdtool_graph['height']        += $size == 'medium' ? 28 : 0 ;
+    $rrdtool_graph['title']          = 'Network';
+    $rrdtool_graph['lower-limit']    = '0';
+    $rrdtool_graph['vertical-label'] = 'Bytes/sec';
+    $rrdtool_graph['extras']         = '--rigid --base 1024';
+
+    if ($context == 'host')
+        $rrdtool_graph['title'] .= " - $hostname";
+
+    $series = "DEF:'bytes_in'='${rrd_dir}/bytes_in.rrd':'sum':AVERAGE "
+       ."DEF:'bytes_out'='${rrd_dir}/bytes_out.rrd':'sum':AVERAGE "
+       ."LINE2:'bytes_in'#$mem_cached_color:'In' "
+       ."LINE2:'bytes_out'#$mem_used_color:'Out' ";
+
+    $rrdtool_graph['series'] = $series;
+
+    return $rrdtool_graph;
+
+}
+
+?>
