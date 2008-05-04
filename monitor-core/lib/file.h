38d407aaafe059a8d4c312d742ceab278da510c2
carenas <carenas>
2008-05-04 10:16:57 +0000

    libganglia: protect against negative offsets for isspace if char is signed

--- monitor-core/lib/file.h
+++ monitor-core/lib/file.h
@@ -7,7 +7,7 @@ ssize_t readn (int fd, void *vptr, size_t n);
 ssize_t writen (int fd, const void *vptr, size_t n);
 #endif
 int slurpfile ( char * filename, char *buffer, int buflen );
-char *skip_whitespace ( const char *p);
-char *skip_token ( const char *p);
+char *skip_whitespace (const char *p);
+char *skip_token (const char *p);
 
 #endif
