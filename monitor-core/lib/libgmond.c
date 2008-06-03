25bf1273e0f40d225c2ea6d34de7bd39fc115123
carenas <carenas>
2008-06-03 11:16:19 +0000

    gmond: add configurable host tmax (timeout max)
    allow setting in gmond a per host configuration with the max time that a
    gmond will wait before considering a host is no longer alive.
    
    the parameter host_tmax is defaulted to 20 seconds for consistency with
    the previous hardcoded value.
    
    Signed-off-by: Sebastien Piechursky <sebastien.piechursky@cggveritas.com>
    Signed-off-by: Carlo Marcelo Arenas Belon <carenas@sajinet.com.pe>

--- monitor-core/lib/libgmond.c
+++ monitor-core/lib/libgmond.c
@@ -57,6 +57,7 @@ static cfg_opt_t globals_opts[] = {
   CFG_BOOL("mute", 0, CFGF_NONE),
   CFG_BOOL("deaf", 0, CFGF_NONE),
   CFG_INT("host_dmax", 0, CFGF_NONE),
+  CFG_INT("host_tmax", 20, CFGF_NONE),
   CFG_INT("cleanup_threshold", 300, CFGF_NONE),
   CFG_BOOL("gexec", 0, CFGF_NONE),
   CFG_INT("send_metadata_interval", 0, CFGF_NONE),
