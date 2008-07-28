98569a6b4078a2305df8c816e574ad27575a8f23
carenas <carenas>
2008-07-28 05:51:45 +0000

    libmetrics: mingw initial support

--- monitor-core/libmetrics/interface.c
+++ monitor-core/libmetrics/interface.c
@@ -3,6 +3,40 @@
 
 #include "interface.h"
 
+#ifdef MINGW
+int
+get_min_mtu( void )
+{
+  DWORD ret, dwInterface, dwSize = 0;
+  PMIB_IFTABLE ifTable;
+  PMIB_IFROW ifRow;
+  unsigned min_mtu = 0;
+
+  dwSize = sizeof(MIB_IFTABLE);
+  ifTable = (PMIB_IFTABLE)malloc(dwSize);
+  while ((ret = GetIfTable(ifTable, &dwSize, 1)) == ERROR_INSUFFICIENT_BUFFER) {
+    ifTable = (PMIB_IFTABLE)realloc(ifTable, dwSize);
+  }
+
+  if (ret = NO_ERROR) {
+    for (dwInterface = 0; dwInterface < (ifTable -> dwNumEntries); dwInterface++) {
+      ifRow = &(ifTable -> table[dwInterface]);
+
+      if ((ifRow -> dwType != MIB_IF_TYPE_LOOPBACK) && (ifRow -> dwOperStatus ==MIB_IF_OPER_STATUS_OPERATIONAL)) {
+        if (min_mtu) {
+	  if (ifRow -> dwMtu < min_mtu) {
+	    min_mtu = ifRow -> dwMtu;
+	  }
+	} else {
+	  min_mtu = ifRow -> dwMtu;
+	}
+      }
+    }
+    free(ifTable);
+  }
+  return min_mtu;
+}
+#else
 int
 get_min_mtu( void )
 {
@@ -28,3 +62,4 @@ get_min_mtu( void )
   free_ifi_info(info);
   return min_mtu;
 }
+#endif
--- monitor-core/libmetrics/interface.h
+++ monitor-core/libmetrics/interface.h
@@ -1,5 +1,14 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H 1
+
+#include "config.h"
+
+#ifdef MINGW
+#include <windows.h>
+#include <iphlpapi.h>
+#else
 #include "unpifi.h"
+#endif
+
 int get_min_mtu( void );
 #endif
