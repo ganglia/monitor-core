3358bcf12ccc4874a203bb46789afeb778d09fad
carenas <carenas>
2008-08-10 02:22:04 +0000

    gmetad: support for C99 in gperf generated files and build instructions
      svn merge -c 1445 trunk
      svn merge -c 1446 trunk
      svn merge -c 1471 trunk
      svn merge -c 1472 trunk
      svn merge -c 1476 trunk
      svn merge -c 1479 trunk
      svn merge -c 1485 trunk
      svn merge -c 1486 trunk
    
    Reviewed-by: Brad Nicholes <BNICHOLES@novell.com>

--- gmetad/type_hash.c
+++ gmetad/type_hash.c
@@ -1,11 +1,43 @@
-/* C code produced by gperf version 2.7.2 */
-/* Command-line: gperf -G -l -H type_hash -t -F ', 0' -N in_type_list -k '1,$' -W types ./type_hash.gperf  */
+/* C code produced by gperf version 3.0.3 */
+/* Command-line: gperf -G -l -H type_hash -t -F ', 0' -N in_type_list -k '1,$' -W types type_hash.gperf  */
+
+#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
+      && ('%' == 37) && ('&' == 38) && ('\'' == 39) && ('(' == 40) \
+      && (')' == 41) && ('*' == 42) && ('+' == 43) && (',' == 44) \
+      && ('-' == 45) && ('.' == 46) && ('/' == 47) && ('0' == 48) \
+      && ('1' == 49) && ('2' == 50) && ('3' == 51) && ('4' == 52) \
+      && ('5' == 53) && ('6' == 54) && ('7' == 55) && ('8' == 56) \
+      && ('9' == 57) && (':' == 58) && (';' == 59) && ('<' == 60) \
+      && ('=' == 61) && ('>' == 62) && ('?' == 63) && ('A' == 65) \
+      && ('B' == 66) && ('C' == 67) && ('D' == 68) && ('E' == 69) \
+      && ('F' == 70) && ('G' == 71) && ('H' == 72) && ('I' == 73) \
+      && ('J' == 74) && ('K' == 75) && ('L' == 76) && ('M' == 77) \
+      && ('N' == 78) && ('O' == 79) && ('P' == 80) && ('Q' == 81) \
+      && ('R' == 82) && ('S' == 83) && ('T' == 84) && ('U' == 85) \
+      && ('V' == 86) && ('W' == 87) && ('X' == 88) && ('Y' == 89) \
+      && ('Z' == 90) && ('[' == 91) && ('\\' == 92) && (']' == 93) \
+      && ('^' == 94) && ('_' == 95) && ('a' == 97) && ('b' == 98) \
+      && ('c' == 99) && ('d' == 100) && ('e' == 101) && ('f' == 102) \
+      && ('g' == 103) && ('h' == 104) && ('i' == 105) && ('j' == 106) \
+      && ('k' == 107) && ('l' == 108) && ('m' == 109) && ('n' == 110) \
+      && ('o' == 111) && ('p' == 112) && ('q' == 113) && ('r' == 114) \
+      && ('s' == 115) && ('t' == 116) && ('u' == 117) && ('v' == 118) \
+      && ('w' == 119) && ('x' == 120) && ('y' == 121) && ('z' == 122) \
+      && ('{' == 123) && ('|' == 124) && ('}' == 125) && ('~' == 126))
+/* The character set is not based on ISO-646.  */
+error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gnu-gperf@gnu.org>."
+#endif
+
+#line 1 "type_hash.gperf"
+
 /* $Id$ */
 /* Recognizes metric types. 
  * Build with: gperf -G -l -H type_hash -t -F ', 0' -N in_type_list -k 1,$ \
  * -W types ./type_hash.gperf > type_hash.c
  */
 #include <gmetad.h>
+#line 9 "type_hash.gperf"
+struct type_tag;
 
 #define TOTAL_KEYWORDS 10
 #define MIN_WORD_LENGTH 4
@@ -38,7 +70,7 @@ type_hash (str, len)
       22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
       22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
       22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      15,  0, 15,  0, 22,  0, 22, 22, 22, 22,
+      10,  5, 15,  0, 22,  0, 22, 22, 22, 22,
       22, 22,  0, 22, 22,  0,  0,  0, 22, 22,
       22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
       22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
@@ -67,23 +99,36 @@ static unsigned char lengthtable[] =
 static struct type_tag types[] =
   {
     {"", 0}, {"", 0}, {"", 0}, {"", 0},
+#line 11 "type_hash.gperf"
     {"int8", INT},
+#line 12 "type_hash.gperf"
     {"uint8", UINT},
+#line 20 "type_hash.gperf"
     {"string", STRING},
     {"", 0}, {"", 0},
+#line 19 "type_hash.gperf"
     {"timestamp", TIMESTAMP},
+#line 13 "type_hash.gperf"
     {"int16", INT},
+#line 14 "type_hash.gperf"
     {"uint16", UINT},
     {"", 0}, {"", 0}, {"", 0},
+#line 15 "type_hash.gperf"
     {"int32", INT},
+#line 16 "type_hash.gperf"
     {"uint32", UINT},
     {"", 0}, {"", 0}, {"", 0},
+#line 17 "type_hash.gperf"
     {"float", FLOAT},
+#line 18 "type_hash.gperf"
     {"double", FLOAT}
   };
 
 #ifdef __GNUC__
 __inline
+#ifdef __GNUC_STDC_INLINE__
+__attribute__ ((__gnu_inline__))
+#endif
 #endif
 struct type_tag *
 in_type_list (str, len)
