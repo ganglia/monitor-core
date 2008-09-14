c015a5a3a20f9e3ea23ff6e5673cdb2d523cb91d
carenas <carenas>
2008-09-14 19:48:05 +0000

    libganglia: consolidate timediff and update_file in file
    migrage libmetrics versions for linux and cygwin as well as multicpu
    DSO to use the common implementation.
    
    timediff made public internally from file.h but should probably move
    to a different header

--- monitor-core/lib/file.c
+++ monitor-core/lib/file.c
@@ -11,6 +11,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include "file.h"
 
 #include "ganglia_priv.h"
 
@@ -149,6 +150,45 @@ read:
 
    close(fd);
    return sl;
+}
+
+float timediff (const struct timeval *thistime,
+                const struct timeval *lasttime)
+{
+  float diff;
+
+  diff = ((double) thistime->tv_sec * 1.0e6 +
+          (double) thistime->tv_usec -
+          (double) lasttime->tv_sec * 1.0e6 -
+          (double) lasttime->tv_usec) / 1.0e6;
+
+  return diff;
+}
+
+char *
+update_file (timely_file *tf)
+{
+    int rval;
+    struct timeval now;
+    char *bp;
+
+    gettimeofday(&now, NULL);
+    if(timediff(&now,&tf->last_read) > tf->thresh) {
+        bp = tf->buffer;
+        rval = slurpfile(tf->name, &bp, tf->buffersize);
+        if(rval == SYNAPSE_FAILURE) {
+            err_msg("update_file() got an error from slurpfile() reading %s",
+                    tf->name);
+        } else {
+            tf->last_read = now;
+            if (tf->buffer == NULL) {
+               tf->buffer = bp;
+               if (rval > tf->buffersize)
+                  tf->buffersize = ((rval/tf->buffersize) + 1) * tf->buffersize;
+            }
+        }
+    }
+    return tf->buffer;
 }
 
 char *
