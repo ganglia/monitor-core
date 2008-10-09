2e86383068e74644aa8125f8c5e04b8c8a119084
twitham <twitham>
2008-10-09 21:45:43 +0000

    Code clean: replace multiple single quotes with one set.

--- monitor-core/web/graph.d/sample_report.php
+++ monitor-core/web/graph.d/sample_report.php
@@ -115,44 +115,44 @@ function graph_sample_report ( &$rrdtool_graph ) {
          * the average
          */
         $series =
-              "DEF:'num_nodes'='${rrd_dir}/cpu_user.rrd':'num':AVERAGE "
-            . "DEF:'cpu_user'='${rrd_dir}/cpu_user.rrd':'sum':AVERAGE "
-            . "CDEF:'ccpu_user'=cpu_user,num_nodes,/ "
-            . "DEF:'cpu_nice'='${rrd_dir}/cpu_nice.rrd':'sum':AVERAGE "
-            . "CDEF:'ccpu_nice'=cpu_nice,num_nodes,/ "
-            . "DEF:'cpu_system'='${rrd_dir}/cpu_system.rrd':'sum':AVERAGE "
-            . "CDEF:'ccpu_system'=cpu_system,num_nodes,/ "
-            . "DEF:'cpu_idle'='${rrd_dir}/cpu_idle.rrd':'sum':AVERAGE "
-            . "CDEF:'ccpu_idle'=cpu_idle,num_nodes,/ "
-            . "AREA:'ccpu_user'#$cpu_user_color:'User CPU' "
-            . "STACK:'ccpu_nice'#$cpu_nice_color:'Nice CPU' "
-            . "STACK:'ccpu_system'#$cpu_system_color:'System CPU' ";
+              "'DEF:num_nodes=${rrd_dir}/cpu_user.rrd:num:AVERAGE' "
+            . "'DEF:cpu_user=${rrd_dir}/cpu_user.rrd:sum:AVERAGE' "
+            . "'CDEF:ccpu_user=cpu_user,num_nodes,/' "
+            . "'DEF:cpu_nice=${rrd_dir}/cpu_nice.rrd:sum:AVERAGE' "
+            . "'CDEF:ccpu_nice=cpu_nice,num_nodes,/' "
+            . "'DEF:cpu_system=${rrd_dir}/cpu_system.rrd:sum:AVERAGE' "
+            . "'CDEF:ccpu_system=cpu_system,num_nodes,/' "
+            . "'DEF:cpu_idle=${rrd_dir}/cpu_idle.rrd:sum:AVERAGE' "
+            . "'CDEF:ccpu_idle=cpu_idle,num_nodes,/' "
+            . "'AREA:ccpu_user#$cpu_user_color:User CPU' "
+            . "'STACK:ccpu_nice#$cpu_nice_color:Nice CPU' "
+            . "'STACK:ccpu_system#$cpu_system_color:System CPU' ";
 
         if (file_exists("$rrd_dir/cpu_wio.rrd")) {
-            $series .= "DEF:'cpu_wio'='${rrd_dir}/cpu_wio.rrd':'sum':AVERAGE "
-                ."CDEF:'ccpu_wio'=cpu_wio,num_nodes,/ "
-                ."STACK:'ccpu_wio'#$cpu_wio_color:'WAIT CPU' ";
+            $series .= "'DEF:cpu_wio=${rrd_dir}/cpu_wio.rrd:sum:AVERAGE' "
+                . "'CDEF:ccpu_wio=cpu_wio,num_nodes,/' "
+                . "'STACK:ccpu_wio#$cpu_wio_color:WAIT CPU' ";
         }
 
-        $series .= "STACK:'ccpu_idle'#$cpu_idle_color:'Idle CPU' ";
+        $series .= "'STACK:ccpu_idle#$cpu_idle_color:Idle CPU' ";
 
     } else {
 
-        // Context is not "host"
-        $series ="DEF:'cpu_user'='${rrd_dir}/cpu_user.rrd':'sum':AVERAGE "
-        . "DEF:'cpu_nice'='${rrd_dir}/cpu_nice.rrd':'sum':AVERAGE "
-        . "DEF:'cpu_system'='${rrd_dir}/cpu_system.rrd':'sum':AVERAGE "
-        . "DEF:'cpu_idle'='${rrd_dir}/cpu_idle.rrd':'sum':AVERAGE "
-        . "AREA:'cpu_user'#$cpu_user_color:'User CPU' "
-        . "STACK:'cpu_nice'#$cpu_nice_color:'Nice CPU' "
-        . "STACK:'cpu_system'#$cpu_system_color:'System CPU' ";
+        // Context is not "'hos't"
+        $series ="'DEF:cpu_user=${rrd_dir}/cpu_user.rrd:sum:AVERAGE' "
+        . "'DEF:cpu_nice=${rrd_dir}/cpu_nice.rrd:sum:AVERAGE' "
+        . "'DEF:cpu_system=${rrd_dir}/cpu_system.rrd:sum:AVERAGE' "
+        . "'DEF:cpu_idle=${rrd_dir}/cpu_idle.rrd:sum:AVERAGE' "
+        . "'AREA:cpu_user#$cpu_user_color:User CPU' "
+        . "'STACK:cpu_nice#$cpu_nice_color:Nice CPU' "
+        . "'STACK:cpu_system#$cpu_system_color:System CPU' ";
 
         if (file_exists("$rrd_dir/cpu_wio.rrd")) {
-            $series .= "DEF:'cpu_wio'='${rrd_dir}/cpu_wio.rrd':'sum':AVERAGE ";
-            $series .= "STACK:'cpu_wio'#$cpu_wio_color:'WAIT CPU' ";
+            $series .= "'DEF:cpu_wio=${rrd_dir}/cpu_wio.rrd:sum:AVERAGE' ";
+            $series .= "'STACK:cpu_wio#$cpu_wio_color:WAIT CPU' ";
         }
 
-        $series .= "STACK:'cpu_idle'#$cpu_idle_color:'Idle CPU' ";
+        $series .= "'STACK:cpu_idle#$cpu_idle_color:Idle CPU' ";
     }
 
     // We have everything now, so add it to the array, and go on our way.
