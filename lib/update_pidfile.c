f969b6932aac4eb7dfa885882aa880e86f12f4b4
carenas <carenas@93a4e39c-3214-0410-bb16-828d8e3bcd0f>
2009-04-06 00:46:29 +0000

    libganglia: split private source daemon_init
    daemon_init() is only used by gmetad while
    update_pidfile() is used also by gmond and
    is therefore better kept in libganglia as
    a private object

---
+++ lib/update_pidfile.c
@@ -0,0 +1,55 @@
+/**
+ * @file update_pidfile.c Functions for standalone daemons
+ */
+/* $Id: daemon_init.c 1986 2009-04-03 13:36:14Z hawson $ */
+#define _XOPEN_SOURCE 500 /* for getpgid */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <gm_msg.h>
+#include "update_pidfile.h"
+
+/**
+ * @fn void update_pidfile (const char *pname)
+ * @param argv0 name of this program
+ */
+void
+update_pidfile (char *pidfile)
+{
+  long p;
+  pid_t pid;
+  mode_t prev_umask;
+  FILE *file;
+
+  /* make sure this program isn't already running. */
+  file = fopen (pidfile, "r");
+  if (file)
+    {
+      if (fscanf(file, "%ld", &p) == 1 && (pid = p) &&
+          (getpgid (pid) > -1))
+        {
+          err_msg("daemon already running: %s pid %ld\n", pidfile, p);
+          exit (1);
+        }
+      fclose (file);
+    }
+
+  /* write the pid of this process to the pidfile */
+  prev_umask = umask (0112);
+  unlink(pidfile);
+
+  file = fopen (pidfile, "w");
+  if (!file)
+    {
+      err_msg("Error writing pidfile '%s' -- %s\n",
+               pidfile, strerror (errno));
+      exit (1);
+    }
+  fprintf (file, "%ld\n", (long) getpid());
+  fclose (file);
+  umask (prev_umask);
+}
---
+++ lib/update_pidfile.h
@@ -0,0 +1,6 @@
+#ifndef UPDATE_PIDFILE_H
+#define UPDATE_PIDFILE_H 1
+
+void update_pidfile (char *pidfile);
+
+#endif
