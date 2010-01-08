daf893f15957e38fe7626820a407000b453b6c99
Daniel Pocock <daniel@pocock.com.au>
2010-01-08 17:17:00 +0000

    Allow space for NUL termination (omitted by r2159)

--- monitor-core/gmetad/rrd_helpers.c
+++ monitor-core/gmetad/rrd_helpers.c
@@ -177,7 +177,7 @@ write_data_to_rrd ( const char *source, const char *host, const char *metric,
                     const char *sum, const char *num, unsigned int step,
                     unsigned int process_time, ganglia_slope_t slope)
 {
-   char rrd[ PATHSIZE ];
+   char rrd[ PATHSIZE + 1 ];
    char *summary_dir = "__SummaryInfo__";
    int i;
 
