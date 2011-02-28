c0deafbe4c223137f28a59a2999ae7915bc58e63
carenas <carenas>
2011-02-28 02:33:27 +0000

    libganglia: remove dead code for readn/writen

--- monitor-core/lib/file.h
+++ monitor-core/lib/file.h
@@ -16,11 +16,6 @@ typedef struct {
   size_t buffersize;
 } timely_file;
 
-/* FreeBSD seems to gag on these.. Yet still works when not compiled in */
-#if defined(BSD)
-ssize_t readn (int fd, void *vptr, size_t n);
-ssize_t writen (int fd, const void *vptr, size_t n);
-#endif
 int slurpfile (char *filename, char **buffer, int buflen);
 float timediff (const struct timeval *thistime,
                 const struct timeval *lasttime);
