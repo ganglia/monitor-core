9f7df6de775656c3895b666f35e71cd7850caa73
bernardli <bernardli>
2011-03-19 00:16:00 +0000

    [web] When $max_graphs is > 0, cluster view is supposed to show up to $max_graphs graphs. This previously didn't work with reports, since they don't have current values to display. This commit fixes that for metric reports by showing "N/A" for hosts that have been cropped off.  If metric columns ($hostcols) is set to 0, the $max_graphs option is ignored.

--- monitor-core/web/cluster_view.php
+++ monitor-core/web/cluster_view.php
@@ -229,11 +229,15 @@ foreach ( $sorted_hosts as $host => $value )
             elseif ($val['TYPE']=="string" or $val['SLOPE']=="zero" or
                     (isset($always_constant[$metricname]) and
                     $always_constant[$metricname] or
-                    ($max_graphs > 0 and $i > $max_graphs )))
+                    ($max_graphs > 0 and $i > $max_graphs and $hostcols != 0)))
                {
-                  $textval = "$val[VAL]";
-                  if (isset($val['UNITS']))
-                     $textval .= " $val[UNITS]";
+                  if (isset($reports[$metricname]) and $reports[$metricname])
+                     // No "current" values available for reports
+                     $textval = "N/A";
+                  else
+                     $textval = "$val[VAL]";
+                     if (isset($val['UNITS']))
+                        $textval .= " $val[UNITS]";
                }
          }
 
