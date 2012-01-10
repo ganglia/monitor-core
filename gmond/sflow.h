973e6c2210a101444f3a594757fd39f4d41ea40d
Neil McKee <neil.mckee@inmon.com>
2012-01-10 05:27:27 -0800

    Enhanced sFlow support (sFlow-HTTP, sFlow-MEMCACHE, sFlow-JVM).  Also improves handling of hypervisors+VMs monitored with sFlow-HOST.

--- gmond/sflow.h
+++ gmond/sflow.h
@@ -27,7 +27,6 @@ extern "C" {
 
 #define SFLOW_MAX_LOST_SAMPLES 7
 #define SFLOW_COUNTERBLOCK_HOST_HID 2000
-#define SFLOW_COUNTERBLOCK_HOST_PARENT 2002
 #define SFLOW_COUNTERBLOCK_HOST_CPU 2003
 #define SFLOW_COUNTERBLOCK_HOST_MEM 2004
 #define SFLOW_COUNTERBLOCK_HOST_DSK 2005
@@ -37,7 +36,12 @@ extern "C" {
 #define SFLOW_COUNTERBLOCK_HOST_VMEM 2102
 #define SFLOW_COUNTERBLOCK_HOST_VDSK 2103
 #define SFLOW_COUNTERBLOCK_HOST_VNIO 2104
-
+#ifdef SFLOW_MEMCACHE_2200
+#define SFLOW_COUNTERBLOCK_MEMCACHE_2200 2200
+#endif
+#define SFLOW_COUNTERBLOCK_MEMCACHE 2204
+#define SFLOW_COUNTERBLOCK_HTTP 2201
+#define SFLOW_COUNTERBLOCK_JVM 2106
 #define SFLOW_MAX_HOSTNAME_LEN 64
 #define SFLOW_MAX_FQDN_LEN 256
 #define SFLOW_MAX_IPSTR_LEN 64
@@ -46,6 +50,9 @@ extern "C" {
 #define SFLOW_MAX_UUIDSTR_LEN 37
 #define SFLOW_MAX_INT32STR_LEN 10
 #define SFLOW_MAX_DSI_LEN (SFLOW_MAX_IPSTR_LEN + SFLOW_MAX_INT32STR_LEN + 1 + SFLOW_MAX_INT32STR_LEN)
+
+#define SFLOW_MAX_METRIC_NAME_LEN 512
+#define SFLOW_MAX_METRIC_PREFIX_LEN 255
 
 typedef struct _SFlowAddr {
   enum { SFLOW_ADDRTYPE_undefined=0, SFLOW_ADDRTYPE_IP4, SFLOW_ADDRTYPE_IP6 } type;
@@ -65,7 +72,7 @@ typedef struct _SFlowXDR {
   /* agent */
   SFlowAddr agentAddr;
   char agentipstr[SFLOW_MAX_IPSTR_LEN];
-  uint32_t agentSubId;
+  uint32_t subAgentId;
   uint32_t uptime_mS;
   /* socket address */
   apr_sockaddr_t *remotesa;
@@ -80,7 +87,6 @@ typedef struct _SFlowXDR {
   /* structure offsets */
   struct {
     uint32_t HID;
-    uint32_t PARENT;
     uint32_t CPU;
     uint32_t MEM;
     uint32_t DSK;
@@ -92,6 +98,12 @@ typedef struct _SFlowXDR {
     uint32_t VNIO;
     uint32_t foundPH;
     uint32_t foundVM;
+#ifdef SFLOW_MEMCACHE_2200
+    uint32_t MEMCACHE_2200;
+#endif
+    uint32_t MEMCACHE;
+    uint32_t HTTP;
+    uint32_t JVM;
   } offset;
 } SFlowXDR;
 
@@ -110,15 +122,6 @@ typedef struct _SFlowXDR {
 #define SFLOWXDR_next_int64(x,pi) do { (*pi) = SFLOWXDR_next(x); (*pi) <<= 32; (*pi) += SFLOWXDR_next(x); } while(0)
 
 typedef struct _SFlowCounterState {
-  apr_time_t last_sample_time;
-  uint32_t datagramSeqNo;
-  uint32_t csSeqNo;
-  uint32_t dsIndex;
-  /* strings */
-  char *osrelease;
-  char *uuidstr;
-  char *dsi;
-  char *parent_dsi;
   /* CPU */
   uint32_t cpu_user;
   uint32_t cpu_nice;
@@ -129,11 +132,13 @@ typedef struct _SFlowCounterState {
   uint32_t cpu_sintr;
   uint32_t interrupts;
   uint32_t contexts;
+
   /* memory */
   uint32_t page_in;
   uint32_t page_out;
   uint32_t swap_in;
   uint32_t swap_out;
+
   /* disk I/O */
   uint32_t reads;
   uint64_t bytes_read;
@@ -172,7 +177,88 @@ typedef struct _SFlowCounterState {
   uint32_t verrs_out;
   uint32_t vdrops_out;
 
+  /* memcache */
+  uint32_t mc_rusage_user;
+  uint32_t mc_rusage_system;
+  uint32_t mc_total_conns;
+  uint32_t mc_cmd_get;
+  uint32_t mc_cmd_set;
+  uint32_t mc_cmd_flush;
+  uint32_t mc_get_hits;
+  uint32_t mc_get_misses;
+  uint32_t mc_delete_misses;
+  uint32_t mc_delete_hits;
+  uint32_t mc_incr_misses;
+  uint32_t mc_incr_hits;
+  uint32_t mc_decr_misses;
+  uint32_t mc_decr_hits;
+  uint32_t mc_cas_misses;
+  uint32_t mc_cas_hits;
+  uint32_t mc_cas_badval;
+  uint32_t mc_auth_cmds;
+  uint32_t mc_auth_errors;
+  uint32_t mc_bytes_read;
+  uint32_t mc_bytes_written;
+  uint32_t mc_conn_yields;
+  uint32_t mc_bytes;
+  uint32_t mc_total_items;
+  uint32_t mc_evictions;
+  uint32_t mc_cmd_touch;
+  uint32_t mc_rejected_conns;
+  uint32_t mc_reclaimed;
+
+  /* HTTP */
+  uint32_t http_meth_option;
+  uint32_t http_meth_get;
+  uint32_t http_meth_head;
+  uint32_t http_meth_post;
+  uint32_t http_meth_put;
+  uint32_t http_meth_delete;
+  uint32_t http_meth_trace;
+  uint32_t http_meth_connect;
+  uint32_t http_meth_other;
+  uint32_t http_status_1XX;
+  uint32_t http_status_2XX;
+  uint32_t http_status_3XX;
+  uint32_t http_status_4XX;
+  uint32_t http_status_5XX;
+  uint32_t http_status_other;
+
+  /* Java JVM */
+  uint32_t jvm_thread_started;
+  uint32_t jvm_gc_count;
+  uint32_t jvm_gc_ms;
+  uint32_t jvm_comp_ms;
+
 } SFlowCounterState;
+
+
+typedef struct _SFlowDataSource {
+  struct _SFlowDataSource *nxt;
+  uint32_t dsClass;
+  uint32_t dsIndex;
+  uint32_t csSeqNo;
+  apr_time_t last_sample_time;
+  /* strings */
+  char *osrelease;
+  char *uuidstr;
+  char *hostname;
+  /* counter state */
+  SFlowCounterState counterState;
+  /* cache metric_prefix string here */
+  char *metric_prefix;
+} SFlowDataSource;
+
+typedef struct _SFlowSubAgent {
+  struct _SFlowSubAgent *nxt;
+  uint32_t subAgentId;
+  uint32_t datagramSeqNo;
+  SFlowDataSource *dataSources;
+} SFlowSubAgent;
+
+typedef struct _SFlowAgent {
+  SFlowSubAgent *subAgents;
+} SFlowAgent;
 
 typedef enum {
 #define SFLOW_GMETRIC(tag,mname,units,slope,format,group,desc,title) tag,
