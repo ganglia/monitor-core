007d248437e49b8e4bb4460f0f028b3c96d1b448
OHamm <oliver.hamm@etu.univ-paris-diderot.fr>
2014-06-16 14:02:03 +0200

    [minor] Not sure about the / APR_TIME_C(1000).

--- gmetad/server.c
+++ gmetad/server.c
@@ -694,7 +694,7 @@ status_report( client_t *client , char *callback)
        hostname,
        gmetad_config.gridname,
        GANGLIA_VERSION_FULL,
-       (long int)(now / APR_TIME_C(1000)), // ms
+       now, // ms
        (long int)(started / APR_TIME_C(1000)), // ms
        (long int)((now - started) / APR_USEC_PER_SEC), // seconds
        (long int)((now - started) / APR_TIME_C(1000)), // ms
